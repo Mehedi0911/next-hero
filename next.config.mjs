d93e653f5e153adf8040d2acf6d282f4fa39f516
mehedi.mosh <mehedi.mosh@gmail.com>
2024-06-02 16:53:58 +0600

    initial build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+               protocol: "https",
+               hostname: "*",
+               port: "",
+               pathname: "/**",
+            },
+         ],
+      },
+      redirects : async () => {
+         return [
+            {
+               
+                  source: '/about',
+                  destination: '/posts',
+                  permanent: true,
+                
+            }
+         ]
+      }
+
+     
+     
+};
 
 export default nextConfig;
